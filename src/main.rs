41c4331568a8d811569dbe66b406832b9607dff6
David <dgreen2@uvm.edu>
2024-04-11 18:32:26 -0400

    updates

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ use std::collections::HashMap;
 // use csv::Writer;
 use flate2::read::GzDecoder;
 use std::env;
-use std::fs::{self, read_dir, File};
-use std::io::{self, BufRead, BufReader, Read};
+use std::fs::{read_dir, File};
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 
 fn main() {
-    // let test_top1 = PathBuf::from("/home/david/Documents/Academic/UVM/Harris_Lab/Mt_sequences");
-    let test_top2 = PathBuf::from("/media/david/WorkDrive/Documents/UVM/HarrisLab/Mt_Data");
+    let test_top1 = PathBuf::from("/home/david/Documents/Academic/UVM/Harris_Lab/Mt_sequences");
+    // let test_top2 = PathBuf::from("/media/david/WorkDrive/Documents/UVM/HarrisLab/Mt_Data");
     let genotype = env::args()
         .nth(1)
         .expect("please enter a valid genome name");
     // let top_dir = env::current_dir().expect("bad top dir");
-    let genomes = create_full_path(test_top2.clone(), String::from("genomes"));
-    let annotation = create_full_path(test_top2.clone(), String::from("annotations"));
+    let genomes = create_full_path(test_top1.clone(), String::from("genomes"));
+    let annotation = create_full_path(test_top1.clone(), String::from("annotations"));
     let dir_geno = get_name(genotype.clone(), genomes.clone());
     let dir_anno = get_name(genotype.clone(), annotation.clone());
     let full_geno = create_full_path(genomes.clone(), dir_geno.clone());
@@ -106,7 +106,6 @@ where
     let mut fasta = HashMap::new();
     let mut curid = String::new();
     let mut curseq = String::new();
-
     for line in buf.lines() {
         let line = line.expect("Could not read line");
         if line.starts_with('>') {
@@ -122,6 +121,27 @@ where
     fasta
 }
 
+fn read_fasta2<P>(filename: P) -> HashMap<String, String>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).expect("oof");
+    let gz = GzDecoder::new(file);
+    let buf = BufReader::new(gz);
+    let mut fasta = HashMap::new();
+    let mut curid = String::new();
+    let mut curseq = String::new();
+    for line in buf.lines() {
+        let line = line.expect("nope");
+        if line.starts_with('>') {
+            if !curid.is_empty() {
+                fasta.inster(curid.clone(), curseq.clone());
+                curseq.clear()
+        }
+        }
+        }
+    }
+}
 fn parse_header(header: String) -> Vec<String> {
     let sp = header.split(' ');
     let mut svec: Vec<String> = Vec::new();
